933e0dec4fb1d8dc2111fa7786e202a603d6955e
Yasin Cluster <cifci@campus.tu-berlin.de>
2023-10-27 12:52:38 +0200

    using tmp instead of temp

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@
 #SBATCH --output=logs/job-%j.out
 
 # 1. copy the squashed dataset to the nodes /tmp 
-rsync -ah --progress /home/myasincifci/data/camelyon17_v1.0.sqfs /temp/
+rsync -ah --progress /home/myasincifci/data/camelyon17_v1.0.sqfs /tmp/
 
-apptainer run --nv -B /temp/camelyon17_v1.0.sqfs:/data/camelyon17_v1.0:image-src=/ ../containers/dispatch.sif python train.py 
+apptainer run --nv -B /tmp/camelyon17_v1.0.sqfs:/data/camelyon17_v1.0:image-src=/ ../containers/dispatch.sif python train.py 
