b536db2dcf31ee2fdd9e5ed2841671f47a1b06fc
Yasin Cifci <cifci@campus.tu-berlin.de>
2023-11-03 17:13:34 +0100

    moved to new dir

--- finetuning/run.sh
+++ finetuning/run.sh
@@ -12,4 +12,4 @@ apptainer run --nv -B /tmp/camelyon17_v1.0.sqfs:/data/camelyon17_v1.0:image-src=
     ../containers/dispatch-new.sif \
     python \
         train.py \
-            --config-name bt_ft_freeze1-3
+            --config-name debug
--- finetuning/train.py
+++ finetuning/train.py
@@ -45,7 +45,7 @@ class DPSmol(pl.LightningModule):
 
     def training_step(self, batch, batch_idx) -> STEP_OUTPUT:
         X, t, M = batch
-        d = self.domain_mapper(self.grouper.metadata_to_group(M.cpu())).cuda()
+        d = self.domain_mapper(self.grouper.metadata_to_group(M.cpu())).to(self.device)
 
         loss_pred, loss_disc = self.model(X, t, d)
 
