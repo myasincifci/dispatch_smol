12efe6e93410c930643b11d697722438a5c3cdb9
Yasin Cluster <cifci@campus.tu-berlin.de>
2023-11-14 19:58:18 +0100

    new bug

--- pretraining/run_iwild.sh
+++ pretraining/run_iwild.sh
@@ -7,6 +7,6 @@
 
 # 1. copy the squashed dataset to the nodes /tmp 
 rsync -ah --progress /home/myasincifci/data/iwildcam_unlabeled_v1.0.sqfs /temp/
-rsync -ah --progress /home/myasincifci/data/iwildcam_v1.0.sqfs /temp/
+rsync -ah --progress /home/myasincifci/data/iwildcam_v2.0.sqfs /temp/
 
-apptainer run --nv -B /temp/iwildcam_unlabeled_v1.0.sqfs:/data/iwildcam_unlabeled_v1.0:image-src=/,/temp/iwildcam_v1.0.sqfs:/data/iwildcam_v1.0:image-src=/ ../../containers/dispatch.sif python train.py --config-name iwildcam
+apptainer run --nv -B /temp/iwildcam_unlabeled_v1.0.sqfs:/data/iwildcam_unlabeled_v1.0:image-src=/,/temp/iwildcam_v2.0.sqfs:/data/iwildcam_v2.0:image-src=/ ../../containers/dispatch.sif python train.py --config-name iwildcam
