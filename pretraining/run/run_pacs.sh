73162883d3690b929fcbd48a94f0e7c45679ab5c
Yasin Cluster <cifci@campus.tu-berlin.de>
2024-04-16 20:23:26 +0200

    increase lr and bs

--- pretraining/run/run_pacs.sh
+++ pretraining/run/run_pacs.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=dispatch-bt
 #SBATCH --partition=gpu-2d
 #SBATCH --gpus-per-node=40gb:1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=4
 #SBATCH --output=logs/job-%j.out
 
 #SBATCH --output=logs/job-%j.out
