70e477410b0e9c20c03a80a1f4bf0db663a64453
Yasin Cluster <cifci@campus.tu-berlin.de>
2024-06-27 17:01:39 +0200

    cluster changes

--- pretraining/train.py
+++ pretraining/train.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from data_modules.camelyon17_dm import CamelyonDM
 from data_modules.pacs_dm import PacsDM
 from data_modules.domainnet_dm import DomainNetDM
-from data_modules.rxrx1_dm import RxRx1DM
+
 from model import BarlowTwins
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning.loggers import WandbLogger
