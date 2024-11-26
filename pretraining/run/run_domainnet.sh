7c302f9f9146fe746ad1c38326f70c0bf1dd3133
Yasin Cluster <cifci@campus.tu-berlin.de>
2024-11-26 11:01:57 +0100

    tweak

--- pretraining/run/run_domainnet.sh
+++ pretraining/run/run_domainnet.sh
@@ -14,6 +14,6 @@
 # 1. copy the squashed dataset to the nodes /tmp 
 rsync -ah --progress /home/myasincifci/data/domainnet_v1.0.sqfs /tmp/
 
-apptainer run --nv -B /tmp/domainnet_v1.0.sqfs:/data/domainnet_v1.0:image-src=/ /home/myasincifci/containers/main/main.sif \
+apptainer run --nv -B /tmp/domainnet_v1.0.sqfs:/data/domainnet:image-src=/ /home/myasincifci/containers/main/main.sif \
     python train.py \
         --config-name domainnet
