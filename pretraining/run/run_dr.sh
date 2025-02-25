e904a6c894f6fa336660dbef21bdab8638412361
Yasin Cluster <cifci@campus.tu-berlin.de>
2025-02-25 11:14:40 +0100

    dr run file

--- pretraining/run/run_dr.sh
+++ pretraining/run/run_dr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=dr-ss
+#SBATCH --job-name=dr-ss-cj
 #SBATCH --partition=gpu-2d
 #SBATCH --gpus-per-node=80gb:1
 #SBATCH --ntasks-per-node=8
@@ -7,11 +7,12 @@
 #SBATCH --exclude=head075
 #SBATCH --mem=256G 
 
-#SBATCH --array=0-0
+#SBATCH --array=0-4
+declare -a seeds=(42 69 666 404 505)
 
 # 1. copy the squashed dataset to the nodes /tmp 
 rsync -ah --progress /home/myasincifci/data/DR.sqfs /tmp
 
 apptainer run --nv -B /tmp/DR.sqfs:/data/DR:image-src=/ /home/myasincifci/containers/main/main.sif \
     python train.py \
-        --config-name dr_debug
+        --config-name dr_cj_scale_100 seed=${seeds[${SLURM_ARRAY_TASK_ID}]} data.color_aug_fct=1.5
